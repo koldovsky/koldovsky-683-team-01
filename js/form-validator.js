23cfe430f81a81667de39403f7210386affa1473
AnastasiaVol1 <nas.volokitina@gmail.com>
2022-04-04 22:26:11 +0000

    add js

--- js/form-validator.js
+++ js/form-validator.js
@@ -1,41 +1,16 @@
-const usernamename = document.getElementById('name');
-const phone = document.getElementById('phone');
-const email = document.getElementById('email');
+let username = document.getElementsByClassName('name')[0];
+let form = document.getElementById('form');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    checkInputs();
-
+// Event listeners
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+    checkIfNameEmpty()
 });
 
-function checkInputs () {
-
-   const userNameValue = name.value.trim();
-   const phoneValue = phone.value.trim();
-   const emailValue = email.value.trim();
-
-
-
-   if(emailValue === ' ') {
-    setErrorFor(email, 'Email cannot be blank');
-   } else if(!isEmail(emailValue)) {
-       setErrorFor(email, 'Email is not valid');
-   } else {
-       setSuccessFor(email);
-   }
-
-
-   }
-
-   function setErrorFor(input, message) {
-       const formControl = input.parentElement;
-   }
-
-   function setSuccessFor(input) {
-       const formControl = input.parentElement;
-   }
-   
-   function isEmail(email) {
-       return /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/.test(email);
-
-   } 
+function checkIfNameEmpty() {
+    if (username.value == '') {
+        username.className = username.className + ' error'
+    } else {
+        username.className = "form_field name"
+    }
+}
